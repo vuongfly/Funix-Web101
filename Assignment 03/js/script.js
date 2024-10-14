f2632f6cbc55eab22a45bc3e9a77725296299dd8
vuongfly <vuongfly96@gmail.com>
2024-10-14 15:48:39 +0700

    Ass 3: add feature

--- Assignment 03/js/script.js
+++ Assignment 03/js/script.js
@@ -1,7 +1,6 @@
 const myEmail = document.getElementById('my-email').textContent
 const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-console.log("hello")
 
 function checkEmail() {
     document.getElementById('input-email-form').classList.add('d-none');
@@ -10,15 +9,22 @@ function checkEmail() {
 
 for (const child of document.getElementById('career-info').children) {
 
-    child.children.item(1).classList.add('d-none');
+    const sectionContent = child.querySelector('.section-content');
+    // Ẩn section-content khi mới load trang
+    sectionContent.classList.add('d-none');
+
     let viewMore = document.getElementById('view-more-btn').cloneNode(true);
     child.append(viewMore);
+    // viewMore.classList.remove('d-none')
+
     child.addEventListener('mouseover', evt => {
         viewMore.classList.remove('d-none')
     })
-    child.addEventListener('mouseout', evt => {
-        viewMore.classList.add('d-none')
-    })
+
+    // child.addEventListener('mouseout', evt => {
+    //     viewMore.classList.add('d-none')
+    // })
+
     child.addEventListener('click', evt => {
         if (viewMore.textContent === 'View more') {
             child.children.item(1).classList.remove('d-none');
@@ -27,6 +33,5 @@ for (const child of document.getElementById('career-info').children) {
             child.children.item(1).classList.add('d-none');
             viewMore.textContent = 'View more'
         }
-
     })
 }
