8bb84a630317ebe90950eb919edc2e7061b3de47
vuongfly <vuongfly96@gmail.com>
2024-11-08 00:15:34 +0700

    fix responsive

--- Assignment 03/js/script.js
+++ Assignment 03/js/script.js
@@ -7,25 +7,27 @@ function checkEmail() {
     document.getElementById('personal-info').classList.remove('d-none');
 }
 
-for (const child of document.getElementById('career-info').children) {
 
-    const section = child.querySelector('.section');
+for (const child of document.getElementsByClassName('section-info')) {
+    console.log(child)
     const sectionContent = child.querySelector('.section-content');
     // Ẩn section-content khi mới load trang
     sectionContent.classList.add('d-none');
 
     let viewMore = document.getElementById('view-more-btn').cloneNode(true);
-    section.append(viewMore);
+    viewMore.classList.remove('d-none')
+    viewMore.classList.add('invisible')
+    child.append(viewMore);
 
     child.addEventListener('mouseover', evt => {
-        viewMore.classList.remove('d-none')
+        viewMore.classList.remove('invisible')
     })
 
     child.addEventListener('mouseout', evt => {
-        viewMore.classList.add('d-none')
+        viewMore.classList.add('invisible')
     })
 
-    child.addEventListener('click', evt => {
+    viewMore.addEventListener('click', evt => {
         if (viewMore.textContent === 'View more') {
             sectionContent.classList.remove('d-none');
             viewMore.textContent = 'View less'
